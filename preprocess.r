76f8be88773d08ccd2cb3ecf2d2a78c67df49971
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2017-06-22 21:15:23 -0400

    Improve metadata

--- preprocess.r
+++ preprocess.r
@@ -2,18 +2,20 @@ library('jsonlite')
 library('stm')
 
 ## There's still something weitd going on with the parsing, most likely around the titles..
+## therefore, limit dataset
 data <- fromJSON( 'data_small.json', simplifyDataFrame=T )
 
-summary( data )
+metadata <- data.frame( data$Date, data$NSF_Org )
+metadata$data.Date <- as.Date( metadata$data.Date )
+metadata$data.NSF_Org <- as.factor( metadata$data.NSF_Org )
+
 
 ## for initial version only like this, change to the fancier one later on
-processed <- textProcessor(Abstract_cleaned, metadata = data)
+processed <- textProcessor( data$Abstract_cleaned, metadata )
 out <- prepDocuments(processed$documents, processed$vocab, processed$meta)
 
 docs <- out$documents
 vocab <- out$vocab
 meta <-out$meta
-
-out <- prepDocuments(processed$documents, processed$vocab, processed$meta, lower.thresh = 50)
 
 save( out, docs, vocab, meta, file = 'processed.rdata' )
