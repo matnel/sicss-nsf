3811d6d9f3463c12f58d091bac83c2ba0bf8b050
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2017-06-22 22:48:36 -0400

    Make predictors to work

--- stm.r
+++ stm.r
@@ -3,14 +3,20 @@ library(stm)
 load('processed.rdata')
 
 model <- stm(
-    documents = out$documents,
-    vocab = out$vocab,
-    K = 0,
+    documents = docs,
+    vocab = vocab,
+    K = 50, ## should be 0
+    prevalence= ~ data.NSF_Org + s( data.Date ),
     max.em.its = 75,
-    data = out$meta,
+    data = meta,
     init.type = "Spectral"
 )
 
 summary( model )
 
 save( model, file = 'model.rdata')
+
+prep <- estimateEffect(1:20 ~ data.NSF_Org + s( data.Date ), model, meta, uncertainty = "Global")
+
+sink( file = "results.txt" )
+summary( prep )
